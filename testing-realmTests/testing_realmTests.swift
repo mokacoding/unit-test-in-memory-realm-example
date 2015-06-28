696e6828c3b5e152e32b742ed8e7382ce36fe002
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-06-28 17:09:44 +1000

    Add Quick and Nimble using Carthage

--- testing-realmTests/testing_realmTests.swift
+++ testing-realmTests/testing_realmTests.swift
@@ -1,36 +1,12 @@
-//
-//  testing_realmTests.swift
-//  testing-realmTests
-//
-//  Created by Giovanni on 28/06/2015.
-//  Copyright (c) 2015 mokagio. All rights reserved.
-//
+import Quick
+import Nimble
 
-import UIKit
-import XCTest
-
-class testing_realmTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+class DemoSpec: QuickSpec {
+    override func spec() {
+        describe("just a demo") {
+            it("demoes the demo") {
+                expect(false).to(beTruthy())
+            }
         }
     }
-    
 }
