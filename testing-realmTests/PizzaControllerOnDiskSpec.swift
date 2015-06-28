0aace62ae920aa04c1b161e71f3a8db98ec21414
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-06-28 21:23:01 +1000

    Add PizzaController spec using on disk Realm

---
+++ testing-realmTests/PizzaControllerOnDiskSpec.swift
@@ -0,0 +1,36 @@
+import Quick
+import Nimble
+import RealmSwift
+import testing_realm
+
+class PizzaControllerOnDiskSpec: QuickSpec {
+    override func spec() {
+        describe("PizzaController") {
+            var testRealm: Realm!
+            var sut: PizzaController!
+
+            beforeEach{
+                testRealm = Realm()
+                sut = PizzaController(realm: testRealm)
+            }
+
+            afterEach {
+                let path = Realm.defaultPath
+                let fileManager = NSFileManager.defaultManager()
+                fileManager.removeItemAtPath(path, error: nil)
+                let lockPath = path + ".lock"
+                fileManager.removeItemAtPath(lockPath, error: nil)
+            }
+
+            it("adds the Pizza to the Realm") {
+                expect(testRealm.objects(Pizza).count).to(equal(0))
+
+                let p = Pizza()
+                p.name = "Margherita"
+                sut.addPizza(p)
+
+                expect(testRealm.objects(Pizza).count).to(equal(1))
+            }
+        }
+    }
+}
